edadcba9c483898410b7e27ef1d14c22b7843bff
Monika Gustilo <monikagustilo@gmail.com>
2022-07-09 22:05:17 -0700

    commit tree bd4694de75a1d1e79834bb47542c879985b98fb8

---
+++ src/00-objectball.js
@@ -0,0 +1,24 @@
+const gameObject = {
+  home: {
+    teamName: "Brooklyn Nets",
+    colors: ["Black", "White"],
+    players: {
+      alanAnderson: {},
+      reggieEvans: {},
+      brookLopez: {},
+      masonPlumlee: {},
+      jasonTerry: {},
+    },
+  },
+  away: {
+    teamName: "Charlotte Hornets",
+    colors: ["Turquiose", "Purple"],
+    players: {
+      jeffAdrien: {},
+      bismakBiyombo: {},
+      deSagnaDiop: {},
+      benGordon: {},
+      brendanHaywood: {},
+    },
+  },
+};
